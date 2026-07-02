3c5e4b4ca049b5e750d09ca3d97502027611b560
Lisapatel21 <lisahiral21@gmail.com>
2026-07-02 17:30:10 +0530

    Add files via upload

---
+++ Python Program.py
@@ -0,0 +1,7 @@
+print("Hello My Name Is Lisa Patel")
+print("I Am Learning AI/ML $ Data Science")
+print("I Have completed My Bachelors From Bhagwan Mahavir University")
+print("I Am Interested In Python Programming")
+print("I Enjoyed Learning New Technologies")
+print("This Is My First Python Program")
+
